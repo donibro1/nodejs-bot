aca7662b13a4c7ca8f01854b61eca05ee80b3b27
donibro1 <mirdoniyorbekj@gmail.com>
2021-12-22 16:00:58 +0500

    Start bot

---
+++ bot.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(process.env.TOKEN, { polling: true })
+bot.on("polling_error", console.log)
+
+bot.on('message', (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Salom, qadirli @" + msg.chat.username)
+})
